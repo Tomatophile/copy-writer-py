38f74dc83898495d7a8190929951cb801448a183
potatophobe <potatophobe@gmail.com>
2022-01-20 22:22:14 +0300

    Add thread class possible to communicate with other

--- app/threads.py
+++ app/threads.py
@@ -4,6 +4,9 @@ from logging import Logger
 from threading import Thread
 from typing import Callable
 
+from app import broker
+from app.broker import Message
+
 logger = Logger('treads')
 
 
@@ -24,3 +27,30 @@ class InterruptableThread(Thread):
 
     def interrupt(self) -> bool:
         return ctypes.pythonapi.PyThreadState_SetAsyncExc(self.get_id(), ctypes.py_object(InterruptedException)) < 1
+
+
+class MessagingThread(InterruptableThread):
+    def __init__(self, queue: str):
+        super().__init__()
+        self.queue = queue
+
+    def run(self) -> None:
+        try:
+            self.init()
+            while True:
+                message = broker.read(self.queue)
+                if message.type == Message.Type.INTERRUPT:
+                    self.interrupt()
+                self.handle_message(message)
+                self.loop()
+        except InterruptedException:
+            pass
+
+    def init(self) -> None:
+        pass
+
+    def handle_message(self, message: Message) -> None:
+        pass
+
+    def loop(self) -> None:
+        pass
